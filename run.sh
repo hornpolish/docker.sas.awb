ef100b2a14eda9174c9bbd193ce2b53b6f5893f5
Paul Kent <paul.kent@sas.com>
2017-11-01 16:16:30 -0400

    fixed container names:tags

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-#docker build -t viya.w47 .
+#docker build -t viya.awb .
 
 docker run -d --name viya -p 8777:8777 -p 17551:17551 -p 7080:7080 -p 8888:8888 -p 5570:5570 --privileged \
        --cap-add SYS_ADMIN -v /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro --rm \
